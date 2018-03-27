89c62105ee3c837d77d769ecfc48f4db4423bf30
Alexandre Bergel <alexandre.bergel@me.com>
2018-03-27 10:17:40 -0300

    removing junk files

--- book/toLatexCompile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pandoc -o book.tex --from markdown --top-level-division=section --toc --toc-depth 1 --template eisvogel --listings --number-sections --listings --top-level-division=chapter \
-	01-introduction/*.markdown \
-	02-Perceptron/*.markdown \
-	03-Neuron/*.markdown \
-	04-NeuralNetwork/*.markdown \
-	XX-GeneticAlgorithm/*.markdown
-
